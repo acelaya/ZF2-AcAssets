4a1f45b38ac528813abf1dc4b6f9edb73b9049e9
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-04-04 22:01:20 +0200

    Creates AssetsOptions service and factory

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,10 @@
 <?php
 return array(
     
-    
+    'service_manager' => array(
+        'factories' => array(
+            'AcAssets\Options\AssetsOptions' => 'AcAssets\Options\Factory\AssetsOptionsFactory',
+        )
+    )
     
 );
---
+++ src/AcAssets/Options/Factory/AssetsOptionsFactory.php
@@ -0,0 +1,27 @@
+<?php
+namespace AcAssets\Options\Factory;
+
+use AcAssets\Options\AssetsOptions;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class AssetsOptionsFactory
+ * @author Alejandro Celaya Alastrué
+ * @see http://www.alejandrocelaya.com
+ */
+class AssetsOptionsFactory implements FactoryInterface {
+
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('Config');
+        return new AssetsOptions(isset($config['assets']) ? $config['assets'] : array());
+    }
+
+}
