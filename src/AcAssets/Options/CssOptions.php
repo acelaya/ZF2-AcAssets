97791f2653bc20198a990183d9bfe7a1430b74f2
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-04-04 22:18:25 +0200

    Defined properties in CssOptions and JsOptions

--- src/AcAssets/Options/CssOptions.php
+++ src/AcAssets/Options/CssOptions.php
@@ -10,4 +10,37 @@ use Zend\Stdlib\AbstractOptions;
  */
 class CssOptions extends AbstractOptions {
 
+    protected $path         = '';
+    protected $stylesheets  = array();
+
+    /**
+     * @param string $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param array $stylesheets
+     */
+    public function setStylesheets($stylesheets)
+    {
+        $this->stylesheets = $stylesheets;
+    }
+    /**
+     * @return array
+     */
+    public function getStylesheets()
+    {
+        return $this->stylesheets;
+    }
+
 } 
--- src/AcAssets/Options/JsOptions.php
+++ src/AcAssets/Options/JsOptions.php
@@ -10,4 +10,53 @@ use Zend\Stdlib\AbstractOptions;
  */
 class JsOptions extends AbstractOptions {
 
+    protected $path     = '';
+    protected $head     = array();
+    protected $inline   = array();
+
+    /**
+     * @param array $head
+     */
+    public function setHead($head)
+    {
+        $this->head = $head;
+    }
+    /**
+     * @return array
+     */
+    public function getHead()
+    {
+        return $this->head;
+    }
+
+    /**
+     * @param array $inline
+     */
+    public function setInline($inline)
+    {
+        $this->inline = $inline;
+    }
+    /**
+     * @return array
+     */
+    public function getInline()
+    {
+        return $this->inline;
+    }
+
+    /**
+     * @param string $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
 } 
