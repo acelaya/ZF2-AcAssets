7b96e283226a472c3b126d128f663b243cc1bc76
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-04-04 22:03:59 +0200

    Creates assets service and factory

--- config/module.config.php
+++ config/module.config.php
@@ -4,6 +4,7 @@ return array(
     'service_manager' => array(
         'factories' => array(
             'AcAssets\Options\AssetsOptions' => 'AcAssets\Options\Factory\AssetsOptionsFactory',
+            'AcAssets\Service\AssetsService' => 'AcAssets\Service\Factory\AssetsServiceFactory',
         )
     )
     
---
+++ src/AcAssets/Service/Factory/AssetsServiceFactory.php
@@ -0,0 +1,33 @@
+<?php
+namespace AcAssets\Service\Factory;
+
+use AcAssets\Options\AssetsOptions;
+use AcAssets\Service\AssetsService;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class AssetsServiceFactory
+ * @author Alejandro Celaya Alastrué
+ * @see http://www.alejandrocelaya.com
+ */
+class AssetsServiceFactory implements FactoryInterface {
+
+    /**
+     * Create service
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return mixed
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        /* @var AssetsOptions $options */
+        $options        = $serviceLocator->get('AcAssets\Options\AssetsOptions');
+        $headScript     = $serviceLocator->get("viewhelpermanager")->get('headscript');
+        $inlineScript   = $serviceLocator->get("viewhelpermanager")->get('inlinescript');
+        $headLink       = $serviceLocator->get("viewhelpermanager")->get('headlink');
+
+        return new AssetsService($headScript, $inlineScript, $headLink, $options);
+    }
+
+}
